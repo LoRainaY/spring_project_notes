fd69d54f0bf79054aa176abab9a4139df0554790
Yana Zaitsava <angels1070@gmail.com>
2022-01-28 16:45:50 +0300

    add userList.ftlh

--- src/main/java/com/zaitsava/spring_project_notes/entity/Role.java
+++ src/main/java/com/zaitsava/spring_project_notes/entity/Role.java
@@ -3,7 +3,7 @@ package com.zaitsava.spring_project_notes.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_USER;
+    ROLE_USER, ROLE_ADMIN;
 
     @Override
     public String getAuthority() {
